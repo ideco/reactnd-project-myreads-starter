3aef3785695d34c60ce136cff31dcc53a26c3cc4
Ivo de Concini <ivo.deconcini@zuehlke.com>
2018-01-30 18:41:35 +0100

    Fetch books from Backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookSearch from './BookSearch'
 import BookList from './BookList'
@@ -7,15 +7,20 @@ import { Route } from 'react-router'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
   }
 
-  
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
 
   render() {
     return (
       <div className="app">
-      <Route exact path="/" component={BookList} />
-      <Route exact path="/search" component={BookSearch} />
+        <Route exact path="/" component={BookList} />
+        <Route exact path="/search" component={BookSearch} />
       </div>
     )
   }
